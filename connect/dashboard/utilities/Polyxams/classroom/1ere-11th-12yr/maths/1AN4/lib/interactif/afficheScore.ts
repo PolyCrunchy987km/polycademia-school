a01cab6e31732926746c1ab283223b315daa2090
PolyCrunchy987km <shauwn.mauri@protonmail.com>
2026-05-02 21:29:16 -1000

    Add files via upload

---
+++ connect/dashboard/utilities/Polyxams/classroom/1ere-11th-12yr/maths/1AN4/lib/interactif/afficheScore.ts
@@ -0,0 +1,28 @@
+import type { IExercice, ResultOfExerciceInteractif } from '../../lib/types'
+
+export function afficheScore(
+  exercice: IExercice,
+  nbBonnesReponses: number,
+  nbMauvaisesReponses: number,
+  divScore?: HTMLDivElement,
+  divButton?: HTMLButtonElement,
+): ResultOfExerciceInteractif {
+  if (divButton != null) {
+    divButton.classList.add(
+      'cursor-not-allowed',
+      'opacity-50',
+      'pointer-events-none',
+    )
+  }
+  if (divScore != null) {
+    divScore.innerHTML = `${nbBonnesReponses} / ${nbBonnesReponses + nbMauvaisesReponses}`
+    divScore.style.color = '#f15929'
+    divScore.style.fontWeight = 'bold'
+    divScore.style.fontSize = 'x-large'
+    divScore.style.display = 'inline'
+  }
+  return {
+    numberOfPoints: nbBonnesReponses,
+    numberOfQuestions: nbBonnesReponses + nbMauvaisesReponses,
+  }
+}
