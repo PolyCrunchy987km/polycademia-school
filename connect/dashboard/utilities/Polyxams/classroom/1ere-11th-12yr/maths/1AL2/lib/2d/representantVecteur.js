0ca4314d95d17600e6dcb98d9bd234d994b29662
Shauwn Mauri <shauwn.mauri@protonmail.com>
2026-05-26 21:13:16 -1000

    Create representantVecteur.js

---
+++ connect/dashboard/utilities/Polyxams/classroom/1ere-11th-12yr/maths/1AL2/lib/2d/representantVecteur.js
@@ -0,0 +1,26 @@
+import { nomVecteurParPosition } from './NomVecteurParPosition';
+import { pointAbstrait } from './PointAbstrait';
+import { segment } from './segmentsVecteurs';
+import { similitude, translation } from './transformations';
+import { milieu } from './utilitairesPoint';
+export function representant(v, A, color = 'black') {
+    const B = pointAbstrait(A.x + v.x, A.y + v.y);
+    const s = segment(A, B, color, '->');
+    s.tailleExtremites = 5;
+    return s;
+}
+export function representantNomme(v, A, nom, taille = 1, color = 'black') {
+    let s;
+    let vTransformed;
+    const B = pointAbstrait(A.x + v.x, A.y + v.y);
+    const M = milieu(A, B);
+    s = segment(A, B, color);
+    const angle = s.angleAvecHorizontale;
+    vTransformed = similitude(v, A, 90, 0.5 / v.norme());
+    if (Math.abs(angle) > 90) {
+        s = segment(B, A, color);
+        vTransformed = similitude(v, A, -90, 0.5 / v.norme());
+    }
+    const N = translation(M, vTransformed);
+    return nomVecteurParPosition(nom, N.x, N.y, taille, 0, color);
+}
