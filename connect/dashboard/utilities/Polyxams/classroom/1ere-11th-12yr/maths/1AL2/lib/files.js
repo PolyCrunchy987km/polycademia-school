dc3ccb39a78f3f1a10269e724fcc0cf6149c6d79
Shauwn Mauri <shauwn.mauri@protonmail.com>
2026-05-26 20:21:59 -1000

    Create files.js

---
+++ connect/dashboard/utilities/Polyxams/classroom/1ere-11th-12yr/maths/1AL2/lib/files.js
@@ -0,0 +1,81 @@
+import { saveAs } from 'file-saver';
+import JSZip from 'jszip';
+import JSZipUtils from 'jszip-utils';
+import { buildImagesUrlsList, doesLatexNeedsPics, getExosContentList, getPicsNames, } from './Latex';
+export function downloadZip(filesUrls, zipFileName) {
+    const zip = new JSZip();
+    let count = 0;
+    filesUrls.forEach((url) => {
+        JSZipUtils.getBinaryContent(url, (err, data) => {
+            if (err) {
+                throw err;
+            }
+            const splitUrl = url.split('/');
+            const fileName = splitUrl[splitUrl.length - 1];
+            zip.file(fileName, data !== null && data !== void 0 ? data : '', { binary: true });
+            count++;
+            if (count === filesUrls.length) {
+                zip.generateAsync({ type: 'blob' }).then((content) => {
+                    saveAs(content, zipFileName);
+                });
+            }
+        });
+    });
+}
+/**
+ * Construit l'archive ZIP contenant le code LaTeX et tous les fichiers images nécessaires pour la compilation du code LaTeX
+ * @param {string} zipFileName nom donné pour l'archive
+ * @param {Latex} latex objet Latex contenant les données des exercices
+ * @param {LatexFileInfos} filesInfo paramètres du fichier LaTeX à générer
+ * @author sylvain
+ */
+export async function downloadTexWithImagesZip(zipFileName, latexFile, exercices) {
+    const zip = new JSZip();
+    const withImages = doesLatexNeedsPics(latexFile.contents);
+    const exosContentList = getExosContentList(exercices);
+    const picsNames = getPicsNames(exosContentList);
+    zip.file('main.tex', latexFile.latexWithPreamble);
+    if (withImages) {
+        const urls = buildImagesUrlsList(exosContentList, picsNames);
+        const imagesFolder = zip.folder('images');
+        let count = 0;
+        urls.forEach((url) => {
+            JSZipUtils.getBinaryContent(url, (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                const splitUrl = url.split('/');
+                const fileName = splitUrl[splitUrl.length - 1];
+                imagesFolder === null || imagesFolder === void 0 ? void 0 : imagesFolder.file(fileName, data !== null && data !== void 0 ? data : '', { binary: true });
+                count++;
+                if (count === urls.length) {
+                    zip.generateAsync({ type: 'blob' }).then((content) => {
+                        // saveAs(content, [archiveName.replace(/\.(?:.*)$/g, ""), "zip"].join("."))
+                        saveAs(content, [zipFileName, 'zip'].join('.'));
+                    });
+                }
+            });
+        });
+    }
+    else {
+        zip.generateAsync({ type: 'blob' }).then((content) => {
+            saveAs(content, [zipFileName, 'zip'].join('.'));
+        });
+    }
+}
+export async function downloadFile(content, fileName) {
+    try {
+        const element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+        element.setAttribute('download', fileName);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    }
+    catch (error) {
+        console.error('Impossible de télécharger le fichier', error);
+        return 'error';
+    }
+    return 'success';
+}
