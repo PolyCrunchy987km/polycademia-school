2cb5b68a419288cae88dae84c1a89ee52a06bd96
Shauwn Mauri <shauwn.mauri@protonmail.com>
2026-06-03 10:30:06 -1000

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Polycademia</title>
-  <link rel="icon" href="https://github.com/PolyCrunchy987km/polycademia-school/blob/main/connect/data/img/logo/app/Polycademia-App-LOGO.png" type="image/png" >
-  <link rel="shortcut icon" href="https://github.com/PolyCrunchy987km/polycademia-school/blob/main/connect/data/img/logo/web/Polycademia-LOGO.png" type="image/png">
+  <link rel="icon" href="https://raw.githubusercontent.com/PolyCrunchy987km/polycademia-school/refs/heads/main/connect/data/img/logo/app/Polycademia-App-LOGO.png" type="image/png" >
+  <link rel="shortcut icon" href="https://raw.githubusercontent.com/PolyCrunchy987km/polycademia-school/refs/heads/main/connect/data/img/logo/app/Polycademia-App-LOGO.png" type="image/png">
   <style>
         /* General reset */
         * {
