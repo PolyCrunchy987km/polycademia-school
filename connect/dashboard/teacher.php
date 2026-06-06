62396e7e2d6329f134958e5475b24c7a49932f77
Shauwn Mauri <shauwn.mauri@protonmail.com>
2026-06-05 18:44:19 -1000

    Update teacher.php

--- connect/dashboard/teacher.php
+++ connect/dashboard/teacher.php
@@ -112,8 +112,8 @@ $myClasses = $teacherClasses[$username] ?? $teacherClasses[array_keys($teacherCl
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="icon" href="https://github.com/PolyCrunchy987km/polycademia-school/blob/main/connect/data/img/logo/live/Polycademia-App-LOGO.svg?raw=true" type="image/svg" >
-<link rel="shortcut icon" href="https://github.com/PolyCrunchy987km/polycademia-school/blob/main/connect/data/img/logo/live/Polycademia-App-LOGO.svg?raw=true" type="image/svg">
+<link rel="icon" href="https://github.com/PolyCrunchy987km/polycademia-school/blob/main/connect/data/img/logo/live/Polycademia-live-LOGO.svg?raw=true" type="image/svg" >
+<link rel="shortcut icon" href="https://github.com/PolyCrunchy987km/polycademia-school/blob/main/connect/data/img/logo/live/Polycademia-live-LOGO.svg?raw=true" type="image/svg">
 <title>Espace Enseignant - Polycademia</title>
 <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 <style>
